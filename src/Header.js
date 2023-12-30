87b2f6c4c8720f93acb0a3dd218a3f57f681756b
blamex321 <laxman2207@outlook.com>
2023-12-30 13:31:37 +0530

    menu to be close automatically if page is slected

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
-import { useState } from 'react';
-import logo from './imgs/logo.png';
-import Menu from './imgs/Menu.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "./imgs/logo.png";
+import Menu from "./imgs/Menu.png";
 
 const Header = () => {
-  const [showHeader, setHeader] = useState(false)
+  const [showHeader, setHeader] = useState(false);
 
   const handleShowHeader = () => {
-    setHeader(!showHeader)
-  }
+    setHeader(!showHeader);
+  };
+
+  const closeMenu = () => {
+    setHeader(false);
+  };
 
   return (
     <header className="app-header">
       <div className="logo-container">
-        <Link to="/">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/" onClick={closeMenu}>
+          <img src={logo} className="App-logo" alt="logo" />
         </Link>
       </div>
       <div className="menu-icon" onClick={handleShowHeader}>
-        <img src={Menu} className='menu-icon' alt="menu" />
+        <img src={Menu} className="menu-icon" alt="menu" />
       </div>
-      <div className={`nav-elements  ${showHeader && 'active'}`}>
+      <div className={`nav-elements  ${showHeader && "active"}`}>
         <nav className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/events">Events</Link>
-          <Link to="/team">Team</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="http://www.google.com" target="_blank" rel="noopener noreferrer">Register</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
+          <Link to="/events" onClick={closeMenu}>
+            Events
+          </Link>
+          <Link to="/team" onClick={closeMenu}>
+            Team
+          </Link>
+          <Link to="/gallery" onClick={closeMenu}>
+            Gallery
+          </Link>
+          <a
+            href="http://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={closeMenu}
+          >
+            Register
+          </a>
         </nav>
       </div>
     </header>
