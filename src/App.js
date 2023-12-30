5ad3493260c603e62bda911c3316eaed9592d9b1
blamex321 <laxman2207@outlook.com>
2023-12-30 13:36:47 +0530

    set footer to the bottom of the screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { loadFull } from "tsparticles";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Snow from "./Snow";
-import Header from './Header';
-import Events from './Events';
-import Team from './Team';
-import Gallery from './Gallery';
-import HomePage from './HomePage';
+import Header from "./Header";
+import Events from "./Events";
+import Team from "./Team";
+import Gallery from "./Gallery";
+import HomePage from "./HomePage";
 import "./App.css";
-import "./Footer"
+import "./Footer";
 import Footer from "./Footer";
-
 
 const App = () => {
   const particlesInit = async (main) => {
@@ -24,20 +23,19 @@ const App = () => {
   };
   return (
     <div className="App">
-      <Snow
-        particlesLoaded={particlesLoaded}
-        particlesInit={particlesInit}
-      />
-      <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<HomePage/>} />
-        <Route path="/events" element={<Events/>} />
-        <Route path="/team" element={<Team/>} />
-        <Route path="/gallery" element={<Gallery/>} />
-      </Routes>
-    </Router>
-    <Footer />
+      <div className="headerDiv">
+        <Snow particlesLoaded={particlesLoaded} particlesInit={particlesInit} />
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </div>
   );
 };
